a01feff16c6256ce4a8f676d048ca0e95ca67282
wileyday <wiley@nurigo.net>
2017-03-16 22:30:19 +0900

    createGroup 구현

--- examples/groupMessage/createGroup.js
+++ examples/groupMessage/createGroup.js
@@ -1,2 +1,23 @@
+/**
+ * vi:set sw=2 ts=2 expandtab:
+ */
+'use strict';
+
 const coolsms = require('../..');
-coolsms.groupMessage.createGroup();
+coolsms.setCredential({
+  apiKey:'NCSBHFS142UDSYOK',
+  apiSecret:'6KLUATUXSUNXCP6WYIEZACJ1FPBJ1AA7'
+});
+
+let options = {
+  appId: null,
+  appVersion: 'JsExample v1.0.0'
+};
+
+coolsms.createGroup(options, function(error, result) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Result:', result);
+  }
+});
--- groupMessage/index.js
+++ groupMessage/index.js
@@ -5,41 +5,58 @@
 
 const request = require('request');
 const url = 'https://solapi.com/GroupMessage/3/createGroup';
+const moment = require('moment-timezone');
 
-function createGroup() {
+let apiKey = null;
+let apiSecret = null;
+const uniqid = require('uniqid');
+const HmacMD5 = require('crypto-js/hmac-md5');
+
+function setCredential(_apiKey, _apiSecret) {
+  apiKey = _apiKey;
+  apiSecret = _apiSecret;
+}
+
+function createGroup(options, callback) {
+  const date = moment.utc().format();
+  const salt = uniqid();
+  const hmacData = date + salt;
+  const signature = HmacMD5(hmacData, apiSecret).toString();
+  let groupOptions = {
+    "appId": null,
+    "osPlatform": "NodeJS",
+    "devLanguage": "NodeJS",
+    "sdkVersion": "JS",
+    "appVersion": null,
+    "siteUser": "__private__",
+    "mode": "real",
+    "forceSms": "false",
+    "onlyAta": "false"
+  };
+
+  groupOptions = Object.assign(groupOptions, options);
+
   request(
     {
       url: url,
       method: 'post',
       headers: {
-        'Authorization': 'HMAC-MD5 ApiKey=NCSBHFS142UDSYOK, Date=2017-03-03T16:46:48+09:00, Salt=SALT1234, Signature=cb6367b2ca2ff12c5463ac193b95ec46'
+        'Authorization': `HMAC-MD5 ApiKey=${apiKey}, Date=${date}, Salt=${salt}, Signature=${signature}`
       },
       json: {
-        "groupOptions": {
-          "appId": "K0123456",
-          "osPlatform": "NodeJS",
-          "devLanguage": "NodeJS",
-          "sdkVersion": "None",
-          "appVersion": "NodeTestClient",
-          "siteUser": "__private__",
-          "mode": "test",
-          "forceSms": "false",
-          "onlyAta": "false",
-        }
+        "groupOptions": groupOptions
       }
     },
     function (error, response, body) {
-      console.log('body');
-      console.log(body);
-      console.log(response.statusCode);
-          if (error) {
-              console.log(error);
-          }
-          if (!error && response.statusCode == 200) {
-              console.log(body)
-          }
+      if (error) {
+          console.log(error);
+      }
+      if (!error && response.statusCode == 200) {
+        callback(error, body);
+      }
     }
   );
 }
 
+module.exports.setCredential = setCredential;
 module.exports.createGroup = createGroup;
